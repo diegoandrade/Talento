9ab1c7d9913686bc1344940daf71d2d1b9284c8b
Diego Andrade <diegoandrade@Diegos-MacBook-Pro-2.local>
2015-01-21 12:22:14 -0500

    Initial Commit

---
+++ Talento/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Talento
+//
+//  Created by Diego Andrade on 1/21/15.
+//  Copyright (c) 2015 Diego Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Talento/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Talento
+//
+//  Created by Diego Andrade on 1/21/15.
+//  Copyright (c) 2015 Diego Andrade. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
